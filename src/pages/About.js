26e3e9895baef0df583b9a96de6b4a00fb0c92fa
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-11-03 18:04:15 +0500

    192 - Cocktails About and Error

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h2 className="section-title">about page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quas ab
+        iste quasi eius ducimus possimus assumenda delectus animi totam
+        repellendus quam asperiores. At placeat nam magni quidem vitae beatae
+        aspernatur non quam cum officia tempore impedit, excepturi quisquam in.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
