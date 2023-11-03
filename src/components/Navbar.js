d88eda4985907ff08353f87d3acb762df2ea907e
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-11-03 11:28:16 +0500

    191 - Cocktails Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../logo.svg'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
-  )
-}
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="cocktail db logo" className="logo"></img>
+        </Link>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <Link to="/about">About</Link>
+        </ul>
+      </div>
+      <h2> Navbar component </h2>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
